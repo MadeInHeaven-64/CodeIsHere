5854f9310f1b1d2a292d4b420fa0724d3eb8292d
Lynx <sofwannuhaalfaruq@gmail.com>
2023-11-14 10:01:56 +0700

    third-commit-update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['index.html', 'login.html', 'about.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: '#001C30',
+        color1: '#176B87',
+        color2: '#64CCC5',
+        color3:'#DAFFFB',
+      },
+    },
   },
   plugins: ['flowbite/plugin'],
 }
