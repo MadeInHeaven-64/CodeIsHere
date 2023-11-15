6cf9e0e59e205293bb688cb8e9bb255099026538
Lynx <sofwannuhaalfaruq@gmail.com>
2023-11-15 14:25:37 +0700

    Second Initial Commit (before update)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        dark: '#001C30',
+        black: '#001C30',
         color1: '#176B87',
         color2: '#64CCC5',
         color3:'#DAFFFB',
